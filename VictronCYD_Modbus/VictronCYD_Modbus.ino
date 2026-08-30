c7e36ccfb20cef270b65261286f74169dec5eadb
Ian Moore <ianofmoore@gmail.com>
2026-08-30 17:17:18 -0500

    fix: repaint hold header immediately

--- VictronCYD_Modbus/VictronCYD_Modbus.ino
+++ VictronCYD_Modbus/VictronCYD_Modbus.ino
@@ -479,7 +479,6 @@ void drawDashboardHeader(uint32_t nowMs, bool fullFrameCleared = false) {
   tft.drawString("GX", 228, 11, 2);
   tft.fillCircle(268, 10, 3, gxOnline ? kGreen : kRed);
 
-  drawDashboardWanIndicator(nowMs, fullFrameCleared);
   tft.fillCircle(216, 10, 2, blink ? kLine : kBackground);
 }
 
@@ -726,15 +725,18 @@ void handleUiAction(const WifiSetupAction& action, uint32_t nowMs) {
 }
 
 void handleTouchAndUiActions(uint32_t nowMs) {
+  bool dashboardInteraction = false;
   TouchEvent event;
   if (touchInputReady && touchInput.poll(event)) {
     switch (event.type) {
       case TouchEventType::Press:
       case TouchEventType::Scroll:
         handleUiAction(wifiSetupUi.handleTouch(event.point, nowMs), nowMs);
+        dashboardInteraction = true;
         break;
       case TouchEventType::Release:
         handleUiAction(wifiSetupUi.handleRelease(nowMs), nowMs);
+        dashboardInteraction = true;
         break;
       case TouchEventType::CalibrationComplete:
         refreshSavedProfiles();
@@ -756,7 +758,9 @@ void handleTouchAndUiActions(uint32_t nowMs) {
       wifiSetupUi.renderDynamic(camperNetwork.status());
       return;
     case DisplaySurface::Dashboard:
-      drawDashboardWanIndicator(nowMs);
+      if (dashboardInteraction || wifiSetupUi.wanHoldCountdown(nowMs) > 0) {
+        drawDashboardHeader(nowMs);
+      }
       return;
   }
 }
